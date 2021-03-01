b6ea0bb3dd0b58c039a1400a90500d2a55ee204d
Amanda <ajnikrant@gmail.com>
2021-03-01 16:30:12 -0500

    commit tree efcf259d83cae3fc79d352b83a7abf5c577fd238

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,32 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.gym== self}
+  end
+
+  def lifters
+    memberships.map{|membership| membership.lifter}
+  end
+
+  def lifter_names
+    memberships.map{|membership| membership.lifter.name}
+  end
+
+  def lifter_totals
+    memberships.map{|membership| membership.lifter.lift_total}.sum
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,20 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+lifter1=Lifter.new("joel", 20)
+lifter2=Lifter.new("mel", 50)
+lifter3=Lifter.new("Cameron", 100)
+
+gym1=Gym.new("Crossfit PR STAR")
+gym2=Gym.new("460 fitness")
+gym3=Gym.new("Gold's gym")
+
+membership1=Membership.new(50, lifter1, gym1)
+membership2=Membership.new(15, lifter2, gym3)
+membership3=Membership.new(200, lifter3, gym1)
+membership4=Membership.new(25, lifter1, gym2)
+membership5=Membership.new(10, lifter1, gym3)
+
 
 binding.pry
 
